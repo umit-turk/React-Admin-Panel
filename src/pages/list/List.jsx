f125e68752ef1cec92130f2eda4185fa690fad00
Umit Turk <uyt.turk@gmail.com>
2022-05-08 19:06:14 +0300

    react router dom

---
+++ src/pages/list/List.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import './list.scss';
+
+function List() {
+  return <div>List</div>;
+}
+
+export default List;
---
+++ src/pages/single/Single.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import './single.scss'
+
+function Single() {
+  return <div>Single</div>;
+}
+
+export default Single;
