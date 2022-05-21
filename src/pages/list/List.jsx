c84d2e9a2fa29169f9c926ce86593e1a01688456
Umit Turk <uyt.turk@gmail.com>
2022-05-21 13:11:24 +0300

    react muı datatable

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import './list.scss';
+import Sidebar from '../../components/sidebar/Sidebar'
+import Navbar from '../../components/navbar/Navbar'
+import Datatable from "../../components/datatable/Datatable";
 
 function List() {
-  return <div>List</div>;
+  return <div className="list">
+    <Sidebar />
+    <div className="listContainer">
+      <Navbar />
+      <Datatable />
+    </div>
+  </div>;
 }
 
 export default List;
